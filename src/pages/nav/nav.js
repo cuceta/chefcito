7cf14ffc2a832f9560550f3c34d11f6c7a6a9622
cuceta <cuceta@oswego.edu>
2025-04-20 14:56:06 -0400

    nav bar formatting/design

--- src/pages/nav/nav.js
+++ src/pages/nav/nav.js
@@ -1,10 +1,22 @@
+// Nav.js
 import React from 'react';
 import "./nav.css"
+import hatLogo from "../../img/hatLogo.png";
 
 const Nav = () => {
     return (
-        <div className="background">
-<h4>hello nav</h4>
+        <div className="nav">
+            <ul className="nav-list">
+                <li className="nav-item"><a href="/breakfast">BREAKFAST</a></li>
+                <li className="nav-item"><a href="/lunch">LUNCH</a></li>
+                <li className="nav-item logo">
+                    <a href="/">
+                        <img src={hatLogo} alt="Logo" className="site-logo" />
+                    </a>
+                </li>
+                <li className="nav-item"><a href="/dinner">DINNER</a></li>
+                <li className="nav-item"><a href="/snack-baking">SNACK/BAKING</a></li>
+            </ul>
         </div>
     );
 };
