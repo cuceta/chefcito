e61b2ac82231208ee4321e5f46f4fa031a4b15d4
cuceta <cuceta@oswego.edu>
2025-04-20 21:17:43 -0400

    Footer scrolling done

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./footer.css";
+import hatAndNameLogo from "../../img/hatAndNameLogoGold.png";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     return (
         <div className="footer">
+            <div className="contact">
 
+            </div>
+
+            <div className="small-logo">
+                <Link to="/home" onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}>
+                    <img src={hatAndNameLogo} alt="hat and chefcito logo"/>
+                </Link>
+            </div>
+
+            <div className="copyright-info">
+
+            </div>
         </div>
     );
 };
--- src/pages/breakfast/breakfast.js
+++ src/pages/breakfast/breakfast.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./breakfast.css";
 import Nav from "../../components/nav/nav";
+import Footer from "../../components/footer/footer";
 
 const Breakfast = () => {
     return (
 
         <div className="breakfast-page">
             <h1>Hello</h1>
+            <Footer/>
+
         </div>
+
     );
 };
 
