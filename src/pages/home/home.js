e33760c724cc5b349d7dbb7ab667b8105bc47feb
cuceta <cuceta@oswego.edu>
2025-04-20 20:46:09 -0400

    Mission and vision section done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -23,6 +23,33 @@ const Home = () => {
 
             <hr/>
 
+            <div className="mission-vision-section">
+                <div className="mission">
+                    <h1 className="sectionHeader">
+                        OUR MISSION
+                    </h1>
+                    <p>
+                        Our mission is to empower busy home cooks by providing easy-to-make, wholesome recipes designed
+                        for meal prep success. We strive to simplify cooking with clear, approachable instructions and
+                        practical tips, ensuring that everyone can enjoy nutritious and delicious meals, save time, and
+                        reduce food waste—even on the busiest days.
+                    </p>
+                </div>
+
+                <div className="vision">
+                    <h1 className="sectionHeader">
+                        OUR VISION
+                    </h1>
+                    <p>
+                        We envision a community where the joy of cooking meets the art of efficient meal preparation.
+                        Our goal is to inspire people worldwide to embrace simple, healthy recipes that transform daily
+                        cooking into a stress-free and sustainable practice, fostering a healthier lifestyle and a closer
+                        connection to the food we share.
+                    </p>
+                </div>
+            </div>
+
+
         </div>
     );
 };
