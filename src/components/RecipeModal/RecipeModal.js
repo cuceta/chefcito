74a8848f910f65a4f49e405c7679670ebdd677ce
cuceta <cuceta@oswego.edu>
2025-04-21 20:32:46 -0400

    breakfast images collected and rendered

--- src/components/RecipeModal/RecipeModal.js
+++ src/components/RecipeModal/RecipeModal.js
@@ -11,7 +11,7 @@ export default function RecipeModal({ recipe, onClose }) {
                 <ul className="recipe-content">{recipe.ingredients.map((i, idx) => <li key={idx}>{i}</li>)}</ul>
                 <div className="detail-line">
                     <h3 className="recipe-section-title ">Allergen Warning: </h3>
-                    <span className="details">{recipe.allergen}</span>
+                    <span className="details">{recipe.allergenWarning}</span>
                 </div>
                 <div className="detail-line">
                     <h3 className="recipe-section-title ">Substitute: </h3>
@@ -22,7 +22,7 @@ export default function RecipeModal({ recipe, onClose }) {
                 <ol className="recipe-content">{recipe.instructions.map((s, idx) => <li key={idx}>{s}</li>)}</ol>
                 <div className="detail-line">
                     <h3 className="recipe-section-title ">Meal Prepping Tip: </h3>
-                    <p className="details">{recipe.tip}</p>
+                    <p className="details">{recipe.mealPreppingTip}</p>
                 </div>
 
             </div>
